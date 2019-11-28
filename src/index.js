2656e0b2ee19732de98df4c8c765f139c5c7bc1b
Andrew Sorensen <Sorensenapaul@gmail.com>
2019-11-28 00:25:10 -0800

    Got basic react stuff going

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './styles/variables.styl';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
